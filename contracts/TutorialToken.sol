33346c8726d3f50ea594386b32d8b50179827c6d
John O. Morales-Marquez <jmarquez.cs@gmail.com>
2018-02-19 15:33:25 -0600

    Token Creation
    Solidity token contract

---
+++ contracts/TutorialToken.sol
@@ -0,0 +1,17 @@
+pragma solidity ^0.4.17;
+
+import 'zeppelin-solidity/contracts/token/ERC20/StandardToken.sol';
+
+contract TutorialToken is StandardToken {
+
+string public name = "One Coin";
+string public symbol = "ONE";
+uint8 public decimals = 10;
+uint public INITIAL_SUPPLY = 1;
+
+function TutorialToken() public {
+  totalSupply_ = INITIAL_SUPPLY;
+  balances[msg.sender] = INITIAL_SUPPLY;
+}
+
+}
