4a0321543006186b3d0b7f7c5660e6c984bd54c0
John O. Morales-Marquez <jmarquez.cs@gmail.com>
2018-02-19 16:18:33 -0600

    Compilation and Migration
    ERC20 Token creating

--- contracts/TutorialToken.sol
+++ contracts/TutorialToken.sol
@@ -3,15 +3,15 @@ pragma solidity ^0.4.17;
 import 'zeppelin-solidity/contracts/token/ERC20/StandardToken.sol';
 
 contract TutorialToken is StandardToken {
-
-string public name = "One Coin";
-string public symbol = "ONE";
-uint8 public decimals = 10;
-uint public INITIAL_SUPPLY = 1;
-
-function TutorialToken() public {
-  totalSupply_ = INITIAL_SUPPLY;
-  balances[msg.sender] = INITIAL_SUPPLY;
+  
+  string public name = "One Coin";
+  string public symbol = "ONE";
+  uint8 public decimals = 10;
+  uint public INITIAL_SUPPLY = 1;
+  
+  function TutorialToken() public {
+    totalSupply_ = INITIAL_SUPPLY;
+    balances[msg.sender] = INITIAL_SUPPLY;
+    }
 }
 
-}
